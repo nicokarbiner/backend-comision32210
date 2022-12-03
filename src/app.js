dabad89709bc931cf20fa8ea9b04ca6062f090ab
Nicolas Karbiner <nkarbiner@gmail.com>
2022-12-03 14:35:37 -0300

    dependencias 4

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-const ProductManager = require('./ProductManager')
-const manager = new ProductManager('products.json');
+const ProductManager = require('./productManager')
+const manager = new ProductManager('./products.json');
 
 
 app.get('/products', async (req, res) => {
