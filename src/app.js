440cb49a7fe5e5bc14903443c6c465032e1ddc82
Nicolas Karbiner <nkarbiner@gmail.com>
2022-12-03 15:10:12 -0300

    dependencias 5

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const app = express();
+const express = require('express')
 
 const ProductManager = require('./productManager')
 const productManager = new ProductManager('./products.json');
 
+const app = express();
 
 app.get('/products', async (req, res) => {
     const products = await productManager.getProducts()
     let limit = req.query.limit
-    if (!limit) res.send({products})
+    if (!limit) res.send({ products })
     else {
         const prodLimit = [];
         if (limit > products.length) limit = products.length;
         for (let index = 0; index < limit; index++) {
             prodLimit.push(products[index]);
         }
-        res.send({prodLimit})
+        res.send({ prodLimit })
     }
 })
 
-app.get('/products/:id', async (req, res) => {
+app.get('/products/:pid', async (req, res) => {
     const id = req.params.pid
     const product = await productManager.getProductById(id)
-    res.send({product})
+    if (product) res.send(product)
+    else res.send({ error: 'No se ha encontrado el producto' })
 })
 
-
-app.listen(8080, ()=>{
-    console.log("Escuchando en el puerto 8080...");
+app.listen(8080, () => {
+    console.log("Sirviendo en el puerto 8080...");
 });
