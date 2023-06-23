67bcb6d199e2a8b8c309199d5d0f44f07d59dc57
Nicolas Karbiner <nkarbiner@gmail.com>
2023-06-23 15:43:31 -0300

    Backend aplicacion ecommerce V.23

--- Backend aplicacion ecommerce/src/routes/sessions.router.js
+++ Backend aplicacion ecommerce/src/routes/sessions.router.js
@@ -12,12 +12,12 @@ import {
 
 const router = Router()
 
-router.post("/register", passportCall("register"), register)
-router.post("/login", passportCall("login"), login)
-router.get("/logout", passportCall("current"), logout)
-router.get("/current", passportCall("current"), getUser)
-router.get( "/github", passport.authenticate("github", { scope: ["user:email"] }), async (req, res) => {})
-router.post("/password_reset", sendRecoveryMail)
-router.put("/password_reset/:uid/:token", changePassword)
+router.post('/register', passportCall('register'), register)
+router.post('/login', passportCall('login'), login)
+router.post('/logout', passportCall('current'), logout)
+router.get('/current', passportCall('current'), getUser)
+router.get('/github', passport.authenticate('github', { scope: ['user:email'] }), async (req, res) => {})
+router.post('/password_reset', sendRecoveryMail)
+router.put('/password_reset/:uid/:token', changePassword)
 
 export default router
